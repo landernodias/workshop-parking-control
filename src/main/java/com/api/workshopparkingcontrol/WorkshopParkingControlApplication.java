7439faa6399d74c76dcc1447e37ceceecf9f61a9
Nelcione Dias <nelcionedias@gmail.com>
2022-10-27 15:07:00 -0300

    create entity PakingSpotModel

--- src/main/java/com/api/workshopparkingcontrol/WorkshopParkingControlApplication.java
+++ src/main/java/com/api/workshopparkingcontrol/WorkshopParkingControlApplication.java
@@ -2,12 +2,19 @@ package com.api.workshopparkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WorkshopParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopParkingControlApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "Olá Mundo";
+	}
 }
