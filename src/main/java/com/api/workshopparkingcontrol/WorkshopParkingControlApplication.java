5d9189db69b19e408043bc4b21b361f9d34c3e42
Nelcione Dias <nelcionedias@gmail.com>
2022-10-27 14:09:48 -0300

    create project

---
+++ src/main/java/com/api/workshopparkingcontrol/WorkshopParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.workshopparkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopParkingControlApplication.class, args);
+	}
+
+}
